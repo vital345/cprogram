b4b1cec75fdf81350321f902cbbfc09298439515
UttamkiniH <53141266+UttamkiniH@users.noreply.github.com>
2020-09-30 19:57:27 +0530

    Update fuction.c

--- fuction.c
+++ fuction.c
@@ -15,4 +15,11 @@ int main()
     printf("num2 = %d", num2);
     return 0;
 }
-
+/*added swap function so that the function can refer the values*/
+void swap(int* n1, int* n2)
+{
+    int temp;
+    temp = *n1;
+    *n1 = *n2;
+    *n2 = temp;
+}
