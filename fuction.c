48c49dce729d71b505ac9c2e8bff72dde070be1b
Hellbat561 <57033880+Hellbat561@users.noreply.github.com>
2021-09-30 21:24:50 +0530

    Same program can be written using calloc()

--- fuction.c
+++ fuction.c
@@ -52,3 +52,29 @@ int main()
     cout << "Marks: " << s.marks << endl;
     return 0;
 }
+
+
+//memory management
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+
+   char name[100];
+   char *description;
+
+   strcpy(name, "Zara Ali");
+
+   /* allocate memory dynamically */
+   description = malloc( 200 * sizeof(char) );
+	
+   if( description == NULL ) {
+      fprintf(stderr, "Error - unable to allocate required memory\n");
+   } else {
+      strcpy( description, "Zara ali a DPS student in class 10th");
+   }
+   
+   printf("Name = %s\n", name );
+   printf("Description: %s\n", description );
+}
