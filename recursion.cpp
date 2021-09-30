91b91c2b7f91e47601beca364c76235ed6199e9f
Hellbat561 <57033880+Hellbat561@users.noreply.github.com>
2021-09-30 21:23:36 +0530

    Create recursion.cpp

---
+++ recursion.cpp
@@ -0,0 +1,26 @@
+//fibanocii series using recursion
+
+#include <stdio.h>
+
+int fibonacci(int i) {
+
+   if(i == 0) {
+      return 0;
+   }
+	
+   if(i == 1) {
+      return 1;
+   }
+   return fibonacci(i-1) + fibonacci(i-2);
+}
+
+int  main() {
+
+   int i;
+	
+   for (i = 0; i < 10; i++) {
+      printf("%d\t\n", fibonacci(i));
+   }
+	
+   return 0;
+}
